a659b077104b102bfddeeff0c784d73cf5d6bc2f
OliviaRamadhani <oliviamdhn@gmail.com>
2024-09-23 09:29:20 +0700

    revisi error npl

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,67 @@
 <?php
 
-use App\Http\Controllers\ItemController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\SettingController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
+
+
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| be assigned to the "api" middleware group. Make something great!
 |
 */
 
-// Halaman awal (bisa dikosongkan atau diarahkan ke dashboard)
+// Authentication Route
+Route::middleware(['auth', 'json'])->prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->withoutMiddleware('auth');
+    Route::delete('logout', [AuthController::class, 'logout']);
+    Route::get('me', [AuthController::class, 'me']);
+});
+
+Route::middleware(['json'])->prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->withoutMiddleware('auth');
+    Route::post('register', [AuthController::class, 'register']); // Rute baru untuk registrasi
+    Route::delete('logout', [AuthController::class, 'logout']);
+    Route::get('me', [AuthController::class, 'me']);
+});
+
+Route::prefix('setting')->group(function () {
+    Route::get('', [SettingController::class, 'index']);
+});
+
+Route::middleware(['auth', 'verified', 'json'])->group(function () {
+    Route::prefix('setting')->middleware('can:setting')->group(function () {
+        Route::post('', [SettingController::class, 'update']);
+    });
+
+    Route::prefix('master')->group(function () {
+        Route::middleware('can:master-user')->group(function () {
+            Route::get('users', [UserController::class, 'get']);
+            Route::post('users', [UserController::class, 'index']);
+            Route::post('users/store', [UserController::class, 'store']);
+            Route::apiResource('users', UserController::class)
+                ->except(['index', 'store'])->scoped(['user' => 'uuid']);
+        });
+
+        Route::middleware('can:master-role')->group(function () {
+            Route::get('roles', [RoleController::class, 'get'])->withoutMiddleware('can:master-role');
+            Route::post('roles', [RoleController::class, 'index']);
+            Route::post('roles/store', [RoleController::class, 'store']);
+            Route::apiResource('roles', RoleController::class)
+                ->except(['index', 'store']);
+        });
+    });
+});
+
 Route::get('/', function () {
     return view('app');
 });
@@ -38,3 +85,4 @@ Route::post('pos-items', [ItemController::class, 'store']);
 Route::get('/{any}', function () {
     return view('app');
 })->where('any', '^(?!api\/)[\/\w\.-]*');
+
